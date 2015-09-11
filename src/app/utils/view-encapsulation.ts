91af48d73f42ff58d50e4dfe2e32dca15c778b0e
Richard Park <objectiv@gmail.com>
2015-09-10 21:23:59 -0400

    fix(utils): ViewEncapsulation member names are no longer all-caps (2.0.0-alpha.37)
    - ViewEncapsulation.NATIVE is now ViewEncapsulation.Native
    - ViewEncapsulation.EMULATED is now ViewEncapsulation.Emulated
    - Update path to module facade/browser

--- src/app/utils/view-encapsulation.ts
+++ src/app/utils/view-encapsulation.ts
@@ -1,6 +1,6 @@
 import { ViewEncapsulation } from 'angular2/angular2';
-import { document } from 'angular2/src/facade/browser';
+import { document } from 'angular2/src/core/facade/browser';
 
 
-const isShadowDomSupported: boolean = Boolean(document && document.body && document.body.createShadowRoot);
-export const encapsulation: ViewEncapsulation = isShadowDomSupported ? ViewEncapsulation.NATIVE : ViewEncapsulation.EMULATED;
+const isShadowDomSupported: boolean = Boolean(document && document.body && document.body['createShadowRoot']);
+export const encapsulation: ViewEncapsulation = isShadowDomSupported ? ViewEncapsulation.Native : ViewEncapsulation.Emulated;
