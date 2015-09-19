420aee552f040d7064b1b81a5071cae6d013cf03
Richard Park <objectiv@gmail.com>
2015-09-18 23:52:45 -0400

    chore(typings): add additional custom angular2 typings

--- typings/custom/angular2/angular2.d.ts
+++ typings/custom/angular2/angular2.d.ts
@@ -1,14 +1,18 @@
-declare module 'angular2/src/core/facade/browser' {
+declare module 'angular2/src/core/facade/async' {
+  export class ObservableWrapper {
+    static subscribe(emitter: any, onNext: Function, onThrow?: Function, onReturn?: Function);
+  }
+}
 
+
+declare module 'angular2/src/core/facade/browser' {
   var document: Document;
   var location: Location;
   var window: Window;
-
 }
 
 
 declare module 'angular2/src/core/facade/lang' {
-
   class Json {
     static parse(s: string): any;
     static stringify(data: Object): string;
@@ -25,5 +29,25 @@ declare module 'angular2/src/core/facade/lang' {
   function isArray(obj: any): boolean;
   function isNumber(obj: any): boolean;
   function isDate(obj: any): boolean;
+}
 
+
+declare module 'angular2/test' {
+  class AsyncTestCompleter {}
+
+  class TestComponentBuilder {
+    createAsync();
+  }
+
+  function afterEach(fn: Function);
+  function beforeEach(fn: Function);
+  function describe(...args);
+  function ddescribe(...args);
+  function xdescribe(...args);
+  function expect(actual: any);
+  function it(name, fn, timeOut?);
+  function iit(name, fn, timeOut?);
+  function xit(name, fn, timeOut?);
+
+  function inject(tokens: any[], fn: Function);
 }
