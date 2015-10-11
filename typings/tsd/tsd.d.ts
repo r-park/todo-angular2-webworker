a692368d893e2a7d10033c4e913590e703ae3655
Richard Park <objectiv@gmail.com>
2015-10-11 00:31:49 -0400

    test(directives): add specs for FocusDirective
    - Add specs for FocusDirective
    - Add sinon to test config, package.json, and typings

--- typings/tsd/tsd.d.ts
+++ typings/tsd/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="angular2/router.d.ts" />
 /// <reference path="es6-shim/es6-shim.d.ts" />
 /// <reference path="jasmine/jasmine.d.ts" />
+/// <reference path="sinon/sinon.d.ts" />
